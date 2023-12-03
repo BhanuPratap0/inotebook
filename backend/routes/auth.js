d1883c80fb38019623e6b6003eb7f06f7cce4ac7
BhanuPratap0 <119663548+BhanuPratap0@users.noreply.github.com>
2023-12-04 00:05:42 +0530

    webtoken done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,22 +3,35 @@ const User = require('../models/User');
 const router=express.Router();
 const {body,validationResult}= require('express-validator');
 
-// Creat a user usin POST:"api/auth". Doesn't require Auth
-router.post('/', [
+// Creat a user usin POST:"api/auth/createuser". Doesn't require Auth
+router.post('/createuser', [
     body('name','Enter a valid name').isLength({min:5}),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({min:5}),
 
-] ,(req,res)=>{
+] ,async(req,res)=>{
+    
    const error=validationResult(req);
    if(!error.isEmpty()){
     return res.status(400).json({error: error.array()});
    }
-   User.create({
+
+   try {
+   let user= await User.findOne({email:req.body.email});
+   if(user){
+    return res.status(400).json({error: "Sorry a user with this email already exists"});
+   }
+   user=await User.create({
     name: req.body.name,
     password: req.body.password,
     email: req.body.email,
-   }).then(user=>res.json(user))
+   })
+   //.then(user=>res.json(user))
+   res.json(user)
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some error occurred");
+}
 
 })
 
